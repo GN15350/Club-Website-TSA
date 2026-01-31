a85516150f2bc94fc1a7b15dca8e309b285ba565
Addison <teardown101@yahoo.com>
2026-01-31 17:29:55 -0600

    new file:   elements/bar.png 	new file:   elements/x.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const header = document.querySelector('.header');
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) {
-            header.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
-        }
-    });
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
 document.getElementById("viewInfo").addEventListener("click", () => {
     document.getElementById("info").scrollIntoView({
         behavior: "smooth"
     });
+});
+
+
+
+
+
+const bar = document.querySelector(".bar");
+const x = document.querySelector(".x");
+const overlay = document.querySelector(".overlay");
+
+bar.addEventListener("click", () => {
+    header.classList.toggle("open");
+    overlay.classList.toggle("show");
+});
+
+x.addEventListener("click", () => {
+    header.classList.remove("open");
+    overlay.classList.remove("show");
 });
