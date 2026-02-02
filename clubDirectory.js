dc7accc4139c568a505b9399c64b2bf27199350a
Addison <teardown101@yahoo.com>
2026-02-01 22:10:16 -0600

    modified:   clubDirectory.css 	modified:   clubDirectory.html 	modified:   clubDirectory.js 	modified:   createClub.css 	modified:   createClub.html 	modified:   createClub.js 	modified:   form.css 	modified:   form.html 	modified:   form.js 	modified:   news.css 	modified:   news.html 	modified:   news.js 	modified:   resources.css 	modified:   resources.html 	modified:   resources.js 	modified:   style.css

--- clubDirectory.js
+++ clubDirectory.js
@@ -68,3 +68,55 @@ fetch("Clubs - Clubs.csv")
     overlay.classList.remove("show");
   });
 });
+
+
+
+
+
+
+const headerBackground = document.querySelector('.header-background');
+
+let scrollPosition = 0;
+let menuOpen = false;
+
+window.addEventListener("scroll", () => {
+    if (window.innerWidth > 600) return;
+    if (menuOpen) return;
+
+    headerBackground.classList.toggle("show", window.scrollY > 50);
+});
+
+const bar = document.getElementById('bar');
+const x = document.getElementById('x');
+const overlay = document.querySelector('.overlay');
+const body = document.body;
+
+function openMenu() {
+    scrollPosition = window.scrollY;
+    menuOpen = true;
+
+    body.style.top = `-${scrollPosition}px`;
+    body.classList.add('no-scroll');
+
+    headerBackground.classList.add('show');
+
+    header.classList.add('open');
+    overlay.classList.add('show');
+}
+
+function closeMenu() {
+    menuOpen = false;
+
+    header.classList.remove('open');
+    overlay.classList.remove('show');
+
+    body.classList.remove('no-scroll');
+    body.style.top = '';
+    window.scrollTo(0, scrollPosition);
+
+    headerBackground.classList.toggle("show", scrollPosition > 50);
+}
+
+bar?.addEventListener('click', openMenu);
+x?.addEventListener('click', closeMenu);
+overlay?.addEventListener('click', closeMenu);
