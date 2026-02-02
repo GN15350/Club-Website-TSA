a7618ffed0fe06eea53fcc2edd5e8acc82f3c684
Addison <teardown101@yahoo.com>
2026-02-01 21:08:46 -0600

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-const header = document.querySelector('.header');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
-
-document.getElementById("viewInfo").addEventListener("click", () => {
-    document.getElementById("info").scrollIntoView({
+document.getElementById("viewInfo")?.addEventListener("click", () => {
+    document.getElementById("info")?.scrollIntoView({
         behavior: "smooth"
     });
 });
 
+const headerBackground = document.querySelector('.header-background');
 
-const headerbackground = document.querySelector('.header-background');
+let scrollPosition = 0;
+let menuOpen = false;
+
 window.addEventListener("scroll", () => {
     if (window.innerWidth > 600) return;
-    headerbackground.classList.toggle("show", window.scrollY > 50);
+    if (menuOpen) return;
+
+    headerBackground.classList.toggle("show", window.scrollY > 50);
 });
 
+const bar = document.getElementById('bar');
+const x = document.getElementById('x');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const body = document.body;
 
+function openMenu() {
+    scrollPosition = window.scrollY;
+    menuOpen = true;
 
+    body.style.top = `-${scrollPosition}px`;
+    body.classList.add('no-scroll');
 
+    headerBackground.classList.add('show');
 
-const bar = document.querySelector(".bar");
-const x = document.querySelector(".x");
-const overlay = document.querySelector(".overlay");
+    header.classList.add('open');
+    overlay.classList.add('show');
+}
 
-bar.addEventListener("click", () => {
-    header.classList.toggle("open");
-    overlay.classList.toggle("show");
-});
+function closeMenu() {
+    menuOpen = false;
 
-x.addEventListener("click", () => {
-    header.classList.remove("open");
-    overlay.classList.remove("show");
-});
+    header.classList.remove('open');
+    overlay.classList.remove('show');
+
+    body.classList.remove('no-scroll');
+    body.style.top = '';
+    window.scrollTo(0, scrollPosition);
+
+    headerBackground.classList.toggle("show", scrollPosition > 50);
+}
+
+bar?.addEventListener('click', openMenu);
+x?.addEventListener('click', closeMenu);
+overlay?.addEventListener('click', closeMenu);
